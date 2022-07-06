ae0e92b994a80d4fbef17247718e75e0a42ef828
Peter Wood <peter.wood@allangray.co.za>
2022-07-06 11:23:36 +0200

    Enable upload of a folders contents

--- index.js
+++ index.js
@@ -2,41 +2,64 @@ import JSZip from "jszip";
 import fs from "fs";
 import path from "path";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { bucketName, fileName } from "./config";
+import { bucketName, fileName, folderName, folderZipName } from "./config.js";
+import archiver from "archiver";
 
 const fileExt = 'txt';
 const filePathToZip = `data/${fileName}.${fileExt}`;
 const fileNameOfZip = `${fileName}.${new Date().getDate()}.zip`;
 
-let zip = zipFile(filePathToZip);
-uploadZipToS3(zip, bucketName, fileNameOfZip)
+process(filePathToZip, fileNameOfZip, bucketName);
 
-function zipFile(filePath){
+
+async function process(filePathToZip, fileNameOfZip, bucketName) {
+    let zipBuffer = await zipFile(filePathToZip);
+    await uploadZipToS3v2(zipBuffer, bucketName, fileNameOfZip);
+
+    zipBuffer = await zipFolder(folderName, uploadToS3Callback);
+}
+
+async function uploadToS3Callback(zipBuffer){
+    await uploadZipToS3v2(zipBuffer, bucketName, folderZipName);
+}
+
+async function zipFile(filePath) {
     let zip = new JSZip();
     const buffer = fs.readFileSync(filePath);
     const fileName = path.basename(filePath);
     zip.file(fileName, buffer);
-    return zip;
+
+    const content = await zip.generateAsync({ type: "nodebuffer" });
+    return content;
 }
 
+async function zipFolder(folderPath, callbackOnSuccess) {
+    const zipName = "folder.zip";
+    const output = fs.createWriteStream(zipName);
 
-function uploadZipToS3(zip, bucketName, objectName) {
-
-    zip.generateAsync({ type: "nodebuffer" }).then(async (content) => {
-        //fs.writeFileSync(objectName, content);
-
-        // Upload ZIP to S3
-        const client = new S3Client({ region: "af-south-1" });
-        const command = new PutObjectCommand({ Body: content, Bucket: bucketName, Key: objectName, ServerSideEncryption: "AES256" });
-        const response = await client.send(command);
-
-        if (response)
-            console.log('success');
-        else
-            console.log('failure');
-
-    }, (reason) => {
-        console.log(reason);
+    output.on('close', function () {
+        callbackOnSuccess(fs.readFileSync(zipName));
+        fs.rm(zipName, () => {console.log("file removed")});
     });
 
+    const archive = archiver("zip");
+    archive.on('error', function(err){
+        throw err;
+    });
+
+    archive.pipe(output);
+    archive.directory(folderPath);
+    await archive.finalize();
+}
+
+async function uploadZipToS3v2(zipBuffer, bucketName, objectName) {
+    // Upload ZIP to S3
+    const client = new S3Client({ region: "af-south-1" });
+    const command = new PutObjectCommand({ Body: zipBuffer, Bucket: bucketName, Key: objectName, ServerSideEncryption: "AES256" });
+    const response = await client.send(command);
+
+    if (response)
+        console.log(`${objectName} uploaded to bucket: ${bucketName}`);
+    else
+        console.log('failure');
 }
